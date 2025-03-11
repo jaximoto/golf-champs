1891f8e0220f6aaf67bba4ef4f857beac6d13e87
jaximoto <62913915+jaximoto@users.noreply.github.com>
2025-03-10 19:10:35 -0700

    Polishing off authentication

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import supabase from "../client";
+import { useNavigate } from "react-router-dom";
 function Dashboard() {
  
+    const navigate = useNavigate();
+    const signOut = async () => {
+      const {error} = await supabase.auth.signOut();
+      if (error) throw error;
+      navigate("/login");
+    }
     return (
-     <h1>This is the Dashboard page</h1>
+     <div><h1>This is the Dashboard page</h1>
+     <button onClick={signOut}>Sign Out</button></div>
     );
   }
   export default Dashboard;
