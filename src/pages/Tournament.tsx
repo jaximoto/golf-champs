5ef789d8203752e7d45c1d4a0f0029b30d5cb94d
yibbyyy <106987734+yibbyyy@users.noreply.github.com>
2025-03-12 02:15:11 -0700

    added scoresheet and tournament

---
+++ src/pages/Tournament.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+//This page will display an individual tournament, present future or past with the relevant information.
+function Tournament(){
+
+    return(
+        <div>
+            <Navbar/>
+            
+        </div>
+    );
+}
+
+export default Tournament
