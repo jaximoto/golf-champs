a700cfa0ceadbbff1fd35e4c62bae5332a2385c0
yibbyyy <106987734+yibbyyy@users.noreply.github.com>
2025-03-20 14:11:01 -0700

    tourney button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import supabase from "../client";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import CreateTournament from "../components/CreateTournament";
 
 function Dashboard() {
 
@@ -10,12 +11,16 @@ function Dashboard() {
     if (error) throw error;
     navigate("/login");
   };
+
+
+  
   return (
 
     <div>
       <Navbar/>
       <h1>This is the Dashboard page</h1>
       <button onClick={signOut}>Sign Out</button>
+      <CreateTournament/>
     </div>
   );
 }
